18c647897a61c6adacd1568e674b57460cfa3a2f
Germanifold <germanifold@outlook.com>
2024-03-30 09:24:01 -0500

    Modifiction of pipeline_registry.py in order  to integrate pipeline_ml_factory model training pipeline.

--- image-classification/src/image_classification/pipeline_registry.py
+++ image-classification/src/image_classification/pipeline_registry.py
@@ -1,9 +1,15 @@
 """Project pipelines."""
+from platform import python_version
 from typing import Dict
 
-from kedro.framework.project import find_pipelines
+from kedro_mlflow.pipeline import pipeline_ml_factory
 from kedro.pipeline import Pipeline
-from .pipelines.model_training import create_pipeline as create_model_training_pipeline
+from image_classification import __version__ as PROJECT_VERSION
+
+
+from kedro.framework.project import find_pipelines
+from .pipelines.data_processing import data_processing as data_engineering_pipeline
+from .pipelines.model_training import training_pipeline as model_training_pipeline
 
 
 
@@ -11,19 +17,33 @@ def register_pipelines() -> Dict[str, Pipeline]:
     """Register the project's pipelines.
 
     Returns:
-        A mapping from pipeline names to ``Pipeline`` objects.
+        A mapping from a pipeline name to a ``Pipeline`` object.
     """
-    # Assuming this function dynamically finds or manually defines pipelines
-    pipelines = {
-        "model_training": create_model_training_pipeline(),
-        # Add other pipelines as needed
+
+    data_processing = data_engineering_pipeline()
+    ml_pipeline = model_training_pipeline()
+    inference_pipeline = ml_pipeline.only_nodes_with_tags("inference")
+     
+    training_pipeline_ml = pipeline_ml_factory(
+        training=ml_pipeline.only_nodes_with_tags("training"),
+        inference=inference_pipeline,
+        input_name="params:prediction_params",
+        log_model_kwargs=dict(
+            artifact_path="image_classification",
+            conda_env={
+                "python": python_version(),
+                "build_dependencies": ["pip"],
+                "dependencies": [f"image_classification=={PROJECT_VERSION}"],
+            },
+            signature="auto",
+        ),
+    )
+
+
+    return {
+        "data_processing": data_processing,
+        "training": training_pipeline_ml,
+        "inference": inference_pipeline,
+        "__default__": data_processing
+        + training_pipeline_ml + inference_pipeline
     }
-
-    # Correctly combine all pipelines for the default pipeline
-    default_pipeline = Pipeline([])
-    for pipeline in pipelines.values():
-        default_pipeline += pipeline
-
-    pipelines["__default__"] = default_pipeline
-
-    return pipelines
